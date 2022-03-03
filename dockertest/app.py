b667a7c7f3a504d5d29ff671d1410f4c270e7e4f
Kerry Kurcz <kurczkerry@gmail.com>
2022-03-03 06:54:20 -0600

    Cloud Run and Container Registry and flask

--- dockertest/app.py
+++ dockertest/app.py
@@ -1,8 +1,16 @@
 from flask import Flask
 
+
+# welcome to the fourth line
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return '<h1>hi world </h1>'
+    return '<h1>oh hi </h1>'
+
+
+if __name__ == "__main__":
+    app.run(ssl_context="adhoc", host="0.0.0.0", port=5000)
